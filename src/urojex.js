dd08d4a6bbf9343a5f994407093418f4c139d65c
HaiNV123 <93862703+HaiNV123@users.noreply.github.com>
2022-08-22 09:33:06 +0700

    add font

--- src/urojex.js
+++ src/urojex.js
@@ -1,5 +1,3 @@
-var fontLink =
-  "@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap')";
 var fontName = "'SFProDisplay', monospace";
 // prettier-ignore
 const colorChanger=(p,c0,c1,l)=>{
@@ -32,9 +30,7 @@ document.head.insertAdjacentHTML(
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../font/stylesheet.css">
   <style>
-  ${fontLink};
   * {
     padding: 0;
     margin: 0;
